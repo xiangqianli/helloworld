7f8d5de95c04ddd07d6c6c6bc1f8a784f0d82d99
lixiangqian <1484937767@qq.com>
2016-09-26 18:52:39 +0800

    DFS走遍天下 递归是王道

--- 90-Subsets II.cpp
+++ 90-Subsets II.cpp
@@ -20,19 +20,21 @@ using namespace std;
 void DFS(vector<vector<int>> *resultlist, vector<int> originlist, vector<int> walk, int path){
     resultlist->push_back(walk);
     for(int i=path; i < originlist.size(); i++){
-        if(walk.size()==0 || originlist[i-1]!=originlist[i]){
-            walk.push_back(originlist[i]);
-            DFS(resultlist, originlist, walk, i+1);
-            walk.pop_back();
-        }
+        //加i>path是因为怕i为0，i-1会报错；另一方面配合后一句主要解决不让同样的数在不同的组合里重复出现。
+        //但是比如1，3，4，4组合，[4，4]是可以出现的，[1,3,4,4]也是可以出现的
+        if(i>path && originlist[i-1]==originlist[i]) continue;
+        walk.push_back(originlist[i]);
+        DFS(resultlist, originlist, walk, i+1);
+        walk.pop_back();
     }
 }
 
 int main(int argc, const char * argv[]) {
-    vector<int> num(3);
+    vector<int> num(4);
     num[0]=1;
-    num[1]=2;
-    num[2]=2;
+    num[1]=4;
+    num[2]=4;
+    num[3]=3;
     sort(num.begin(), num.end());
     vector<vector<int>> resultlist;
     vector<int> walk;
