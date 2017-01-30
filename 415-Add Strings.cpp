e44d0bb0c9c8d6fc9ad9241426a9e437fd76b17d
lixiangqian <1484937767@qq.com>
2017-01-30 09:37:53 +0800

    字符串，熟悉stl函数，和int间的转换

---
+++ 415-Add Strings.cpp
@@ -0,0 +1,51 @@
+//
+//  415-Add Strings.cpp
+//  leecode
+//
+//  Created by lixiangqian on 17/1/29.
+//  Copyright © 2017年 lixiangqian. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+string addStrings(string num1, string num2) {
+    long len1 = num1.size();
+    long len2 = num2.size();
+    string result = "";
+    bool up = false;
+    for (long i = 0; i < max(len1, len2); i++) {
+        int l = i<len1?num1[len1 - i - 1]-'0':0;
+        int n = i<len2?num2[len2 - i - 1]-'0':0;
+        
+        if (up) {
+            result = to_string((l+ n + 1)%10) + result;
+            up = (l+n+1)>=10;
+        }else{
+            result = to_string((l+ n)%10) + result;
+            up = (l+n)>=10;
+        }
+    }
+    
+    if (up) {
+        result = "1"+result;
+    }
+    return result;
+}
+int main(){
+    string num1 = "111111";
+    string num2 = "999";
+    string result = addStrings(num1, num2);
+    cout<< result<<endl;
+    cout<<"hello world!"<<endl;
+    return 0;
+}
