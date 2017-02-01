3ef7ae15881d7a4cf62fd1a6e778c088fd65dba5
lixiangqian <1484937767@qq.com>
2017-02-01 21:22:55 +0800

    数组，vector set erase,find,count方法，效率很低

---
+++ 349-IntersectionofTwoArrays.cpp
@@ -0,0 +1,47 @@
+//
+//  349-IntersectionofTwoArrays.cpp
+//  leetcode
+//  erase会从set/map/vector里把那个迭代器指向的空间销掉，后面的元素都要往前移一位；返回下一个元素的迭代地址
+//  Created by lixiangqian on 17/2/1.
+//  Copyright © 2017年 lixiangqian. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> result;
+    if (nums1.size() == 0 || nums1.size() == 0) {
+        return result;
+    }
+    set<int> e(nums1.begin(), nums2.end());
+    for (int j = 0; j < nums2.size(); j++) {
+        if (find(e.begin(), e.end(), nums2[j])!=e.end()) {
+            result.push_back(nums2[j]);
+            e.erase(nums2[j]);
+        }
+    }
+    return result;
+}
+
+int main(){
+    vector<int> nums1(2);
+    nums1[0] = 1;
+    nums1[1] = 2;
+    vector<int> nums2(1);
+    nums2[0] = 1;
+    vector<int> result = intersection(nums1, nums2);
+    for (long i =0; i<result.size(); i++) {
+        cout<<result[i]<<endl;
+    }
+    cout<<"hello world!"<<endl;
+    return 0;
+}
