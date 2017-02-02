2ff40e00cf0836a56fd0252129bd0f3a6c48ef11
lixiangqian <1484937767@qq.com>
2017-02-02 23:19:08 +0800

    数组，排序好的，移走重复元素，返回新长度，很easy，可以举一反三下其它情况

---
+++ 26-Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,39 @@
+//
+//  26-Remove Duplicates from Sorted Array.cpp
+//  leecode
+//
+//  Created by lixiangqian on 17/2/2.
+//  Copyright © 2017年 lixiangqian. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+int removeDuplicates(vector<int>& nums) {
+    int s = (int)nums.size();
+    for (int i =1 ; i<nums.size(); i++) {
+        if (nums[i] == nums[i-1]) {
+            s--;
+        }
+    }
+    return s;
+}
+int main(){
+    vector<int> nums(4);
+    nums[0] = 1;
+    nums[1] = 3;
+    nums[2] = 3;
+    nums[3] = 5;
+    int result = removeDuplicates(nums);
+    cout<< result <<endl;
+    cout<<"hello world!"<<endl;
+    return 0;
+}
