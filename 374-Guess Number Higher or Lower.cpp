451137fb3080616dbdd30538d3e9ad0a7b3f1142
lixiangqian <1484937767@qq.com>
2017-02-03 14:30:33 +0800

    二分查找，low和high两个指针，找到返回mid，没找到返回low

---
+++ 374-Guess Number Higher or Lower.cpp
@@ -0,0 +1,50 @@
+//
+//  374-Guess Number Higher or Lower.cpp
+//  leecode
+//
+//  Created by lixiangqian on 17/2/3.
+//  Copyright © 2017年 lixiangqian. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+int guess(int num){
+    int n = 7;
+    if (num == n) {
+        return 0;
+    }else if(num > n){
+        return -1;
+    }else
+        return 1;
+}
+
+int guessNumber(int n) {
+    int low = 1, high= n, mid ;
+    while (low < high) {
+        mid = low + (high-low)/2;
+        if (guess(mid)==0) {
+            return mid;
+        }else if(guess(mid) > 0){
+            low = mid +1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return low;
+}
+
+int main(){
+    int result = guessNumber(10);
+    cout<< result <<endl;
+    cout<<"hello world!"<<endl;
+    return 0;
+}
